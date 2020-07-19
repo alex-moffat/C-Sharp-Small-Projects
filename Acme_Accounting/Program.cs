f8222430277a1822490e26227b619507f99e08b1
Alex Moffat <wamoffat4@gmail.com>
2020-07-19 15:03:00 -0700

    Create Acme Accounting program
    Print to console:
    - Welcome to Acme Accounting Systems
    - Remember, we’re “accounting” on you!

---
+++ Acme_Accounting/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Acme_Accounting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems\nRemember, we\'re \"accounting\" on you!");
+            Console.ReadLine();
+        }
+    }
+}
