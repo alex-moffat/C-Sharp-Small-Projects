6260b56e3c294b166fb62dd2fd550e022c591c71
Alex Moffat <wamoffat4@gmail.com>
2020-07-19 18:17:45 -0700

    Create Daily Report program
    - Console program.
    - Print header.
    - Ask questions.
    - Save data according smallest proper data type w/ clearly labeled variables.
    - Print thank you statement

---
+++ Daily_Report/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Daily_Report
+{
+    class Program
+    {
+        static void Main()
+        {
+            //----- GREETING
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+            //----- NAME
+            Console.WriteLine("\nWhat is your name?");
+            string iName = Console.ReadLine();
+            //----- COURSE
+            Console.WriteLine("\nWhat course are you on?");
+            string iCourse = Console.ReadLine();
+            //----- PAGE
+            Console.WriteLine("\nWhat page number?");
+            ushort iPage = Convert.ToUInt16(Console.ReadLine());
+            //----- HELP
+            Console.WriteLine("\nDo you need help with anything? Please answer only \"true\" or \"false\".");
+            bool iHelp = Convert.ToBoolean(Console.ReadLine());
+            //----- POSITIVE
+            Console.WriteLine("\nWere there any positive experiences you’d like to share? Please give specifics.");
+            string iPositive = Console.ReadLine();
+            //----- FEEDBACK
+            Console.WriteLine("\nIs there any other feedback you’d like to provide? Please be specific.");
+            string iFeedback = Console.ReadLine();
+            //----- HOURS
+            Console.WriteLine("\nHow many hours did you study today?");
+            float iHours = float.Parse(Console.ReadLine());
+            //----- EXIT
+            Console.WriteLine("\nThank you for your answers. An Instructor will respond to this shortly. Have a great day!");
+            //Console.WriteLine(iName + "\n" + iCourse + "\n" + iPage + "\n" + iHelp + "\n" + iPositive + "\n" + iFeedback + "\n" + iHours);
+            Console.ReadLine();
+        }
+    }
+}
