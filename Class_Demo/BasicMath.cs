c7553ea5e9ae34110bbca7627d943398d932dbda
Alex Moffat <wamoffat4@gmail.com>
2020-07-23 15:47:48 -0700

    Create Class_Demo
    - Create a class.
    - Create three methods in the class that take two integer parameters
    - Methods perform addition, multiplication, and power opertions
    - Ask the user what numbers they want to do the math operations with.
    - Call each method, passing the user input to the method.
    - Display the returned integer/decimal to the screen.

---
+++ Class_Demo/BasicMath.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Demo
+{
+    public class BasicMath
+    {
+                
+        //========== ADD method
+        public int Add(int num1, int num2)
+        {
+            int value = num1 + num2;
+            return value;
+        }
+        //========== MULTIPLY method
+        public int Multiply(int num1, int num2)
+        {
+            int value = num1 * num2;
+            return value;
+        }
+        //========== POWER method
+        public decimal Power(int num1, int num2)
+        {
+            decimal value = (decimal)Math.Pow(num1, num2);
+            return value;
+        }
+    }
+}
---
+++ Class_Demo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===== CREATE CLASS OBJECT
+            BasicMath basic = new BasicMath();
+            
+            //===== GET USER INPUT
+            bool valid = false;
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("Pick a number to do some math with:");
+                    int pick1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Pick a another number to do some math with:");
+                    int pick2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("{0:n0} + {1:n0} = {2:n0}", pick1, pick2, basic.Add(pick1, pick2));
+                    Console.WriteLine("{0:n0} x {1:n0} = {2:n0}", pick1, pick2, basic.Multiply(pick1, pick2));
+                    Console.WriteLine("{0:n0} to the power of {1:n0} = {2:n0}", pick1, pick2, basic.Power(pick1, pick2));
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oops...something went wrong:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //===== HOLD OPEN - till enter is pressed
+            Console.ReadLine();
+        }
+    }
+}
