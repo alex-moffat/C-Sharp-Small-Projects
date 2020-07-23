224d6ab778a6976824dee77a7969e23482556752
Alex Moffat <wamoffat4@gmail.com>
2020-07-22 20:20:27 -0700

    Create Exception_Handling Demo
    - Create a list of integers.
    - Ask the user for a number to divide each number in the list by.
    - Create loop that divides each list number by the number the user entered and displays the result to the screen.
    - Put the loop in a try/catch block.
    - Print a message after program has emerged from the try/catch block.

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            //=============== EXAMPLES ===============
+            List<int> numbers = new List<int> { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97 };
+            try
+            {
+                Console.WriteLine("Pick a number to divide a list of numbers by:");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                foreach (int n in numbers)
+                {
+                    Console.WriteLine(n / num1);
+                }                
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Make sure to select a whole number");
+                Console.WriteLine(e.Message);
+                
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Please don't divide by zero");
+                Console.WriteLine(e.Message);
+                
+            }
+            catch (Exception e)
+            {
+                //----- this will trigger if you enter a number too large for integer
+                Console.WriteLine("Oops...something went wrong");
+                Console.WriteLine(e.Message);                
+            }
+            finally 
+            {
+                Console.WriteLine("\nfinally triggered...Log something to a DB etc.");
+            }
+            Console.WriteLine("Emerged from the try/catch block");
+            
+            //===== HOLD OPEN - till enter is pressed
+            Console.ReadLine();
+        }
+    }
+}
