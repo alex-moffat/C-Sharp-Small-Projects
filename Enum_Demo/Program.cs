325d47e73467abfb6ed4ea5b9567214d6ce9bf67
Alex Moffat <wamoffat4@gmail.com>
2020-07-27 20:22:38 -0700

    Create Enum_Demo
    - Create an enum for the days of the week.
    - Prompt the user to enter the current day of the week.
    - Assign the value to a variable of that enum data type created.
    - Use a try/catch block  to check if user enters a day of the week and print to console if error occurs.
    + Use Enum.tryparse() method to check if user entered a valid day of the week.
    + Use system enum DayOfWeek to check day enter by user against actual day of the week.
    + Message user for etiher outcome.
    + Format enum to string & use string format methods

---
+++ Enum_Demo/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum_Demo
+{
+    class Program
+    {
+        //===== ENUM - custom variable
+        enum DaysOfWeek { monday, tuesday, wednesday, thursday, friday, saturday, sunday }
+        static void Main(string[] args)
+        {
+            //===== CONSOLE COLOR
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            DayOfWeek wk = DateTime.Today.DayOfWeek; // system enum variable
+            Console.WriteLine(wk);
+
+            Console.WriteLine("\n========== ENUM DEMO ==========");
+            //===== USER INPUT
+            bool valid = false;
+            string strInput;
+            DaysOfWeek theDay;
+            //---VALIDATE - using try/except method
+            Console.WriteLine("\n===== USING try/catch method");
+            while (!valid)
+            {
+                try
+                {
+                    Console.WriteLine("\nEnter the current day of the week:");
+                    strInput = Console.ReadLine().ToLower();
+                    theDay = (DaysOfWeek)Enum.Parse(typeof(DaysOfWeek), strInput);
+                    Console.WriteLine("You picked {0}.", char.ToUpper(theDay.ToString()[0]) + theDay.ToString().Substring(1));
+                    valid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Please enter an actual day of the week:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            //---VALIDATE - using tryparse() method
+            Console.WriteLine("\n===== USING tryparse() method");
+            valid = false;
+            while (!valid)
+            {
+                Console.WriteLine("\nEnter the current day of the week:");
+                strInput = Console.ReadLine().ToLower();
+                if (Enum.TryParse(strInput, true, out theDay))
+                {
+                    if (wk.ToString().ToLower() == theDay.ToString())
+                    {
+                        Console.WriteLine("{0} == {1} = {3}", wk, theDay, wk.ToString() == theDay.ToString());
+                        valid = true;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Today is not {0}. Try again.", char.ToUpper(theDay.ToString()[0]) + theDay.ToString().Substring(1));
+                    }
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Please enter an actual day of the week:");
+                }                    
+            }
+
+            //===== HOLD OPEN - till enter is pressed
+            Console.ReadLine();
+        }
+    }
+}
