33bdf393b1e2eba6628a9e04ea5bb1bb75af42d8
Alex Moffat <wamoffat4@gmail.com>
2020-07-31 14:51:18 -0700

    Create File_IO
    A CONSOLE APP:
    - Ask the user for a number.
    - Log that number to a text file.
    - Print the text file back to the user.
    + Error handling for user input

---
+++ File_IO/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace File_IO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //===== CONSOLE COLOR
+            Console.BackgroundColor = ConsoleColor.DarkGray;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Clear();
+
+            //========== LOG FILE
+            string logTxt = "========== File_IO LOG ==========\n";
+            string logDir = Directory.GetCurrentDirectory() + @"\logs";
+            string logFile = logDir + @"\log.txt";
+            if (!Directory.Exists(logDir))
+            {
+                Directory.CreateDirectory(logDir);                
+                File.WriteAllText(logFile, logTxt);
+            }
+            
+            //========== USER INPUT
+            bool isValid = false;
+            while (!isValid)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a number:");
+                    int num = Convert.ToInt32(Console.ReadLine());
+                    logTxt = num.ToString();
+                    isValid = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Oops...something went wrong:");
+                    Console.WriteLine(e.Message);
+                }
+            }
+
+            //========== WRITE TO FILE
+            //--- method 1
+            using (StreamWriter file = new StreamWriter(logFile, append: true))
+            {
+                file.WriteLine(logTxt);
+            }
+            //--- method 2
+            File.AppendAllText(logFile, logTxt);
+
+            //========== WRITE TO FILE
+            Console.WriteLine(File.ReadAllText(logFile));
+
+            //===== HOLD OPEN - till enter is pressed
+            Console.ReadLine();
+        }
+    }    
+}
