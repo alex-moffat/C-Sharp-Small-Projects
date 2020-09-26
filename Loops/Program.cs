872f39e6340a318fd31d17784fa8cb417754ad52
Alex Moffat <wamoffat4@gmail.com>
2020-09-25 17:31:54 -0700

    Screenshot - Loops

--- Loops/Program.cs
+++ Loops/Program.cs
@@ -64,68 +64,68 @@ namespace Loops
 
             //=============== EXAMPLES ===============
 
-            ////===== WHILE - performs action after the while condition is evaluated
-            //ushort gNum = 0;
-            //bool isGuessed = false;
-            //while  (!isGuessed)
-            //{
-            //    Console.WriteLine("Guess a number from 1 to 100:");
-            //    gNum = Convert.ToUInt16(Console.ReadLine());
-            //    switch (gNum)
-            //    {
-            //        case 12:
-            //            Console.WriteLine("You guessed number {0}. That is correct!", gNum);
-            //            isGuessed = true;
-            //            break;
-            //        case 29:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            break;
-            //        case 55:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            break;
-            //        case 62:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            break;
-            //        default:
-            //            Console.WriteLine("You are just plain wrong!\n", gNum);
-            //            break;
-            //    }                
-            //}
-            ////===== DO WHILE - performs action before the while condition is evaluated
-            //Console.WriteLine("Guess a number from 1 to 100:");
-            //gNum = Convert.ToUInt16(Console.ReadLine());
-            //isGuessed = gNum == 12;
-            //do
-            //{
-            //    switch (gNum)
-            //    {
-            //        case 12:
-            //            Console.WriteLine("You guessed number {0}. That is correct!", gNum);
-            //            isGuessed = true;
-            //            break;
-            //        case 29:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            Console.WriteLine("Guess a number from 1 to 100:");
-            //            gNum = Convert.ToUInt16(Console.ReadLine());
-            //            break;
-            //        case 55:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            Console.WriteLine("Guess a number from 1 to 100:");
-            //            gNum = Convert.ToUInt16(Console.ReadLine());
-            //            break;
-            //        case 62:
-            //            Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
-            //            Console.WriteLine("Guess a number from 1 to 100:");
-            //            gNum = Convert.ToUInt16(Console.ReadLine());
-            //            break;
-            //        default:
-            //            Console.WriteLine("You are just plain wrong!\n", gNum);
-            //            Console.WriteLine("Guess a number from 1 to 100:");
-            //            gNum = Convert.ToUInt16(Console.ReadLine());
-            //            break;
-            //    }
-            //}
-            //while (!isGuessed);
+            //===== WHILE - performs action after the while condition is evaluated
+            ushort gNum = 0;
+            bool isGuessed = false;
+            while  (!isGuessed)
+            {
+                Console.WriteLine("Guess a number from 1 to 100:");
+                gNum = Convert.ToUInt16(Console.ReadLine());
+                switch (gNum)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed number {0}. That is correct!", gNum);
+                        isGuessed = true;
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        break;
+                    case 62:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        break;
+                    default:
+                        Console.WriteLine("You are just plain wrong!\n", gNum);
+                        break;
+                }                
+            }
+            //===== DO WHILE - performs action before the while condition is evaluated
+            Console.WriteLine("Guess a number from 1 to 100:");
+            gNum = Convert.ToUInt16(Console.ReadLine());
+            isGuessed = gNum == 12;
+            do
+            {
+                switch (gNum)
+                {
+                    case 12:
+                        Console.WriteLine("You guessed number {0}. That is correct!", gNum);
+                        isGuessed = true;
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        Console.WriteLine("Guess a number from 1 to 100:");
+                        gNum = Convert.ToUInt16(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        Console.WriteLine("Guess a number from 1 to 100:");
+                        gNum = Convert.ToUInt16(Console.ReadLine());
+                        break;
+                    case 62:
+                        Console.WriteLine("You guessed number {0}. Try again.\n", gNum);
+                        Console.WriteLine("Guess a number from 1 to 100:");
+                        gNum = Convert.ToUInt16(Console.ReadLine());
+                        break;
+                    default:
+                        Console.WriteLine("You are just plain wrong!\n", gNum);
+                        Console.WriteLine("Guess a number from 1 to 100:");
+                        gNum = Convert.ToUInt16(Console.ReadLine());
+                        break;
+                }
+            }
+            while (!isGuessed);
 
             //===== HOLD - console open till enter pressed
             Console.ReadLine();
